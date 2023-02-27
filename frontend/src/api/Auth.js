2a90673481271335ca034059c79b7102425bd05b
umang3x5 <patel3x5@uwindsor.ca>
2023-02-26 23:44:16 -0500

    added services changes

--- frontend/src/api/Auth.js
+++ frontend/src/api/Auth.js
@@ -1,9 +1,21 @@
 // import { getApiCall, postApiCall } from '../utils/Api';
 
-export const loginUser = async () => {};
+export const loginUser = async (data) => {
+  // const result = await postApiCall('/login', data);
+  // return result.data;
+};
 
-export const registerUser = async () => {};
+export const registerUser = async (data) => {
+  // const result = await postApiCall('/signup', data);
+  // return result.data;
+};
 
-export const validateSession = async () => {};
+export const validateSession = async () => {
+  // const result = await getApiCall('/validate-session');
+  // return result.data;
+};
 
-export const logoutUser = async () => {};
+export const logoutUser = async () => {
+  // const result = await getApiCall('/logout');
+  // return result.data;
+};
